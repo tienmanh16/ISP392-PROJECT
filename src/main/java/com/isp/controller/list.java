4e39977feaaf231d2d09788c78f6af118159e688
Nguyen Huu Huy <nguyenhuy8829@gmail.com>
2024-05-21 12:00:23 +0700

    thu

--- src/main/java/com/isp/controller/list.java
+++ src/main/java/com/isp/controller/list.java
@@ -2,17 +2,24 @@ package com.isp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("loc")
 public class list {
 
-    @GetMapping("/index")
+    @GetMapping("/room")
     public String listRoom() {
-        return "index";
+        return "room.html";
     }
 
     @GetMapping("/detail")
     public String detailR() {
-        return "detail";
+        return "detail.html";
+    }
+
+@GetMapping("test")
+    public String room(){
+        return "test";
     }
 }
