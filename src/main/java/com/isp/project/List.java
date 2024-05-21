fcad44cf31f4f25ed99747595d8089d3b9722588
Loc Nguyen <locnt@gmail.com>
2024-05-21 14:16:01 +0700

    'update3'

---
+++ src/main/java/com/isp/project/List.java
@@ -0,0 +1,18 @@
+package com.isp.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class List {
+    @GetMapping("/index")
+    public String listRoom() {
+        return "index";
+    }
+
+    @GetMapping("/detail")
+    public String detailR() {
+        return "detail";
+    }
+
+}
