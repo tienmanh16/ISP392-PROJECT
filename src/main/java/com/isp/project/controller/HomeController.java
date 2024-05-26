89a17e64c42b3e0a96a58e41fe1cba8a48ba4fcf
TatLoc119 <tatloc110903@gmail.com>
2024-05-26 21:57:28 +0700

    listroom

--- src/main/java/com/isp/project/controller/HomeController.java
+++ src/main/java/com/isp/project/controller/HomeController.java
@@ -1,15 +1,23 @@
 package com.isp.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.isp.project.service.RoomService;
 
 @Controller
 @RequestMapping("")
 public class HomeController {
 
+    @Autowired
+    private RoomService roomService;
+
     @GetMapping("/room")
-    public String listRoom() {
+    public String listRoom(Model model) {
+        model.addAttribute("rooms", roomService.getAllRoomsWithDetails());
         return "room";
     }
 
---
+++ src/main/java/com/isp/project/model/ImageDetail.java
@@ -0,0 +1,27 @@
+package com.isp.project.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ImageDetail")
+public class ImageDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ImageID", nullable = false)
+    private int id;
+
+
+    @Column(name = "Image1", nullable = false)
+    private String image1;
+
+    @Column(name = "Image2", nullable = false)
+    private String image2;
+
+}
