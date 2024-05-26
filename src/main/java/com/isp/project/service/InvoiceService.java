498d71b33b665ea0be529dad89c1f72dd3f3ed78
TatLoc119 <tatloc110903@gmail.com>
2024-05-26 23:40:25 +0700

    update cuoi

---
+++ src/main/java/com/isp/project/service/InvoiceService.java
@@ -0,0 +1,10 @@
+package com.isp.project.service;
+
+import java.util.List;
+
+import com.isp.project.model.Invoice;
+
+public interface InvoiceService {
+    List<Invoice> getAllInvoice();
+    
+}
