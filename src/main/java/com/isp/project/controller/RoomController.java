13f34af36cbea9e0e09cbead638029a341057d96
Nguyen Manh Tien <tienmanh1609jike@gmail.com>
2024-06-19 01:27:20 +0700

    merge loc, viet loi js

--- src/main/java/com/isp/project/controller/RoomController.java
+++ src/main/java/com/isp/project/controller/RoomController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.isp.project.dto.RoomCustomerDTO;
 import com.isp.project.dto.RoomDetailDTO;
