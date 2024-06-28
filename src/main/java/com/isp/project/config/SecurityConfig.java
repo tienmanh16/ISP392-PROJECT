32cff9470e7345bc1c0fcc62a3ac9bfd1b08a423
Nguyen Manh Tien <tienmanh1609jike@gmail.com>
2024-06-28 08:22:42 +0700

    done merge viet1

--- src/main/java/com/isp/project/config/SecurityConfig.java
+++ src/main/java/com/isp/project/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/receptionist/**").hasAnyRole("RECEPTIONIST", "ADMIN")
                 .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers("/**").permitAll()
+                .requestMatchers("/home", "/contact", "/forgotpass/**", "/detail", "/css/**", "/images/**", "/scss/**", "/fonts/**", "/js/**","/assets/**", "/login/**").permitAll()
             )
             .formLogin(form -> form
                 .loginPage("/login")
