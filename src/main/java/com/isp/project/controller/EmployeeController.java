51622bdb85b27eeca593368df1f4f41383aef5a4
Nguyen Huu Huy <nguyenhuy8829@gmail.com>
2024-07-23 00:15:38 +0700

    huy done update profile

--- src/main/java/com/isp/project/controller/EmployeeController.java
+++ src/main/java/com/isp/project/controller/EmployeeController.java
@@ -70,6 +70,7 @@ public class EmployeeController {
         if (bindingResult.hasErrors()) {
             return "addEm";
         }
+        employee.setRole("ROLE_RECEPTIONIST");
         employee.setIsActive(true);
         employeeService.saveUser(employee);
         return "redirect:/admin/employee_list";
@@ -105,11 +106,73 @@ public class EmployeeController {
             existingEmployee.setPhone(employee.getPhone());
             existingEmployee.setDob(employee.getDob());
             existingEmployee.setSalary(employee.getSalary());
-            existingEmployee.setRole(employee.getRole());
+
             employeeService.saveUser(existingEmployee);
         }
         return "redirect:/admin/employee_list";
 
+    }
+
+    @GetMapping("/employee_editPro/{id}")
+    public String editPro(Model model, @PathVariable("id") int id) {
+        Employee employee = employeeService.findById(id);
+        if (employee != null) {
+            model.addAttribute("employee", employee);
+            return "editProbyAd";
+        } else {
+            return "redirect:/admin/viewProfile";
+        }
+    }
+
+    @PostMapping("/employee_editPro")
+    public String updatePro(Model model, @Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "editProbyAd";
+        }
+
+        Employee existingEmployee = employeeService.findById(employee.getId());
+        if (existingEmployee != null) {
+            
+            existingEmployee.setFullName(employee.getFullName());
+            existingEmployee.setGender(employee.getGender());
+            existingEmployee.setAddress(employee.getAddress());
+           
+            existingEmployee.setIdenId(employee.getIdenId());
+            existingEmployee.setUsername(employee.getUsername());
+            
+            existingEmployee.setPhone(employee.getPhone());
+            existingEmployee.setDob(employee.getDob());
+            // Không cập nhật lại salary và role
+            // existingEmployee.setSalary(employee.getSalary());
+            // existingEmployee.setRole(employee.getRole());
+            employeeService.saveUser(existingEmployee);
+        }
+        return "redirect:/admin/viewProfile";
+    }
+
+    @GetMapping("/employee_edit_email/{id}")
+    public String editEmail(Model model, @PathVariable("id") int id) {
+        Employee employee = employeeService.findById(id);
+        if (employee != null) {
+            model.addAttribute("employee", employee);
+            return "editProEmailbyAd";
+        } else {
+            return "redirect:/admin/viewProfile";
+        }
+    }
+
+    @PostMapping("/employee_edit_email")
+    public String updateEmail(Model model, @Valid @ModelAttribute("employee") Employee employee, @RequestParam("password") String password, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "editProEmailbyAd";
+        }
+        Employee existingEmployee = employeeService.findById(employee.getId());
+        if (existingEmployee != null) {
+            existingEmployee.setEmail(employee.getEmail());
+            existingEmployee.setPassword(employee.getPassword());
+            employeeService.saveUser(existingEmployee);
+        }
+        return "redirect:/logout";
     }
 
     @GetMapping("/employee_delete/{id}")
